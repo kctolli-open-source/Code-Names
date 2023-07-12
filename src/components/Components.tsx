c16ac2595582feaea7274338ed8e837e1dc06fdf
kctolli <developer@ktolliver.org>
2023-07-11 23:56:30 -0600

    don't make api call

--- src/components/Components.tsx
+++ src/components/Components.tsx
@@ -2,15 +2,16 @@
 import React, { useState, useEffect, Suspense } from 'react';
 import dynamic from 'next/dynamic';
 
-import ApiHelper from '@/common/apiHelper';
-//import words from '@/common/words/words';
+//import ApiHelper from '@/common/apiHelper';
+import words from '@/common/words/words';
 
 export default function Components() {
     const Board = dynamic(() => import('@/components/Board/Board'));
     const Selection = dynamic(() => import('@/components/Selection/Selection'));
 
     const [data, setData]: any = useState<any>();
-    useEffect(() => {ApiHelper.fetchData('words', setData)}, ['words']);
+    //useEffect(() => {ApiHelper.fetchData('words', setData)}, ['words']);
+    useEffect(() => {setData(words)}, []);
 
     return (
         <Suspense fallback={<Board elements={(new Array(25).fill(""))} />}>
